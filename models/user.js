e836f3c07c52eb7def0eadde2c5dabab105d2e2f
atanda uthamn <atandauthman2@gmail.com>
2022-10-10 18:18:08 +0100

    dvd

--- models/user.js
+++ models/user.js
@@ -2,10 +2,25 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
-    firstName: { type: String, required: true, unique: false },
-    lastName: { type: String, required: true, unique: false },
-    username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
+    firstName: {
+      type: String,
+      required: [true, "must provide name, name cannot be empty"],
+      unique: false,
+    },
+    lastName: {
+      type: String,
+      required: [true, "must provide name, name cannot be empty"],
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: [true, "username already exists"],
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: [true, "email already exists"],
+    },
     password: { type: String, required: true },
     isAdmin: {
       type: Boolean,
