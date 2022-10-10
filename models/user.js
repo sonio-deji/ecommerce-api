094435e176378e72ace224b201ad7b6cdb4ed9d5
atanda uthamn <atandauthman2@gmail.com>
2022-10-10 17:43:23 +0100

    removed unique firstname and last name

--- models/user.js
+++ models/user.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
-    firstName: { type: String, required: true, unique: true },
-    lastName: { type: String, required: true, unique: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
